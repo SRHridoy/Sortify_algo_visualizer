6b0af065dab082ca23441bdb6fb5b9affe624414
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2024-01-31 18:29:00 +0600

    Normal design for testing functionality...

---
+++ app/src/main/java/com/elitcoder/sortify/searchingActivities/LinearSearchMainActivity.java
@@ -0,0 +1,14 @@
+package com.elitcoder.sortify.searchingActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.elitcoder.sortify.R;
+
+public class LinearSearchMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_search_main);
+    }
+}
